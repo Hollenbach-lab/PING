ebb387c29fbe894e1329e29f1e5d6ac39715428d
wesleymarin <wesley.marin185@gmail.com>
2020-04-10 18:30:13 -0700

    getting PING2_run.R ready for distribution

--- PING2_run.R
+++ PING2_run.R
@@ -1,5 +1,4 @@
 
-
 setwd('/home/LAB_PROJECTS/PING2_PAPER/PING2') #Set this to your own PING2 working directory
 
 source('Resources/general_functions.R') # do not change
@@ -10,27 +9,27 @@ source('ping_copy.R') # do not change
 rawFastqDirectory <- '/home/LAB_PROJECTS/PING2_PAPER/3_script_results/extractedFastq/' # can be set to raw sequence or extractedFastq directory
 fastqPattern <- '_KIR_' # use '_KIR_' to find already extracted files, otherwise use 'fastq' or whatever fits your data
 threads <- 12
-resultsDirectory <- '/home/LAB_PROJECTS/PING2_PAPER/3_script_results/'
+resultsDirectory <- '/home/LAB_PROJECTS/PING2_PAPER/3_script_results/' # Set the master results directory (all pipeline output will be recorded here)
 shortNameDelim <- '_' # can set a delimiter to shorten sample ID's (ID will be characters before delim)
 
 
 # Preparation -------------------------------------------------------------
 # Build up a list of sample objects
-sampleList <- general.paired_sample_objects(rawFastqDirectory, fastqPattern, resultsDirectory, shortNameDelim)
+sampleList <- general.paired_sample_objects(rawFastqDirectory, fastqPattern, resultsDirectory, shortNameDelim) # no need to change
 
 
 # PING2 extractor ---------------------------------------------------------
 cat('\n\n----- Moving to PING2 KIR extraction -----')
 # Define the extracted fastq directory
-extractedFastqDirectory <- file.path(resultsDirectory,'extractedFastq')
+extractedFastqDirectory <- file.path(resultsDirectory,'extractedFastq') # no need to change
 # Run PING2 extractor
-sampleList <- extractor.run(sampleList,threads,extractedFastqDirectory,forceRun=F)
+sampleList <- extractor.run(sampleList,threads,extractedFastqDirectory,forceRun=F) # set forceRun=T if you want to force alignments
 
 
 # PING2 gene content and copy number --------------------------------------
 cat('\n\n----- Moving to PING2 gene content and copy determination -----')
-sampleList <- ping_copy.graph(sampleList=sampleList,threads=threads,resultsDirectory=resultsDirectory,forceRun=F)
-sampleList <- ping_copy.manual_threshold(sampleList=sampleList,resultsDirectory=resultsDirectory) #use this function to set copy thresholds
+sampleList <- ping_copy.graph(sampleList=sampleList,threads=threads,resultsDirectory=resultsDirectory,forceRun=F) # set forceRun=T if you want to force alignments
+sampleList <- ping_copy.manual_threshold(sampleList=sampleList,resultsDirectory=resultsDirectory) # this function sets copy thresholds
 
 ## Seeing the following message when generating copy plots is normal
 'A line object has been specified, but lines is not in the mode
