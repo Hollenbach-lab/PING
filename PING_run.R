bb51f7cc15907f3cd52bd38c7c42755a9d48fc6a
wesleymarin <wesley.marin185@gmail.com>
2021-01-01 12:32:02 -0800

    Update PING_run.R
    Setting default use.thresh = F

--- PING_run.R
+++ PING_run.R
@@ -86,8 +86,8 @@ sampleList <- ping_copy.graph(sampleList=sampleList,threads=threads,resultsDirec
 # 
 # ' 6.12 hours for 50 samples at 40 threads, 150bp, 50dp
 # '
-sampleList <- ping_copy.manual_threshold(sampleList=sampleList,resultsDirectory=resultsDirectory,use.threshFile = T) # this function sets copy thresholds
-sampleList <- ping_copy.load_copy_results( sampleList, resultsDirectory )
+sampleList <- ping_copy.manual_threshold(sampleList=sampleList,resultsDirectory=outDir$path,use.threshFile = F) # this function sets copy thresholds
+sampleList <- ping_copy.load_copy_results( sampleList, outDir$path )
 # 
 # # Fix for poor 2DL2 
 # #sapply(sampleList, function(x) x$copyNumber[['KIR2DL2']] <- as.character(2-as.integer(x$copyNumber[['KIR2DL3']])))
