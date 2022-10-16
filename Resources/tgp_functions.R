dff2366a98d5763dd98651e0b23f4a8850e120a1
wesleymarin <wesley.marin185@gmail.com>
2022-10-16 14:34:04 -0700

    Fixing wget download typo

--- Resources/tgp_functions.R
+++ Resources/tgp_functions.R
@@ -10,7 +10,7 @@ retrieve_ref <- function(resourceDir){
     cat('\nDownloading ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa')
     cat('to',resourceDir)
     cat('This might take awhile...')
-    downloadRef <- system2('wget', c(paste('-o',refPath), 
+    downloadRef <- system2('wget', c(paste('-O',refPath), 
                                      "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa"))
     check.system2_output(downloadRef, 'reference download failed')
     cat('-- DONE! --')
@@ -20,11 +20,11 @@ retrieve_cram <- function(sampleID, output_dir, ref_path, bed_path, resourceDir)
   output_path <- file.path(output_dir,paste0(sampleID,'_KIR.cram'))
   
   if( file.exists(output_path) ){
-    cat(paste('\n\t',output_path,'already exists, skipping this download..'))
+    print(paste('\n\t',output_path,'already exists, skipping this download..'))
     return(output_path)}
   
   data_url <- get_tgp_url(sampleID, dataManifest, awsUrl)
-  cat(paste('\n\t',data_url,'->',output_path))
+  print(paste('\n\t',data_url,'->',output_path))
   downloadCram <- system2('samtools',c('view',
                                        paste('--reference',ref_path),
                                        '-M',
@@ -32,7 +32,6 @@ retrieve_cram <- function(sampleID, output_dir, ref_path, bed_path, resourceDir)
                                        paste('-o',output_path),
                                        data_url))
   check.system2_output(downloadCram, paste(sampleID,'CRAM download failed:',data_url))
-  file.remove(sampleID,'.final.cram.crai')
   return(output_path)}
 
 cram_to_fq <- function(sampleID, cram_path, output_path){
@@ -41,7 +40,7 @@ cram_to_fq <- function(sampleID, cram_path, output_path){
   f2_path <- file.path(output_path,paste0(sampleID,'_KIR_2.fq'))
   
   if( file.exists(f1_path) & file.exists(f2_path) ){
-    cat(paste('\n\t',f1_path,'already exists, skipping this conversion..'))
+    print(paste('\n\t',f1_path,'already exists, skipping this conversion..'))
     return(list('f1'=f1_path,'f2'=f2_path))}
   
   cram2fq <- system2('samtools',c('sort','-n','-M',
